257e6938f58fcbb399786ab63f22d2c1ac336bbe
honami-matida <yuzuyuzu202405@gmail.com>
2024-05-16 10:15:55 +0000

    [Finish]4-11

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  resources :post_images, only: [:new, :index, :show]
   devise_for :users
   get 'homes/about' => 'homes#about',as: 'about'
   root to: "homes#top"
-  resources :post_images, only: [:new, :create, :inede, :show]
+  resources :post_images, only: [:new, :create, :index, :show]
   #resources :lists
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
