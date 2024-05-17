12fca29cb3f50bda123b8d01a8a26ef85e9db6b6
honami-matida <yuzuyuzu202405@gmail.com>
2024-05-17 15:21:01 +0000

    [Finish]4-21

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -4,6 +4,13 @@ class PostImage < ApplicationRecord
 
   has_one_attached :image
   belongs_to :user
+  has_many :post_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+
+#shop_nameが存在しているかを確認するバリデーション
+  #imageが存在しているかを確認するバリデーション
+  validates :shop_name, presence: true
+  validates :image, presence: true
 
   def get_image
     unless image.attached?
@@ -11,6 +18,10 @@ class PostImage < ApplicationRecord
       image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
     image
+  end
+
+  def favorited_by?(user)
+    favorites.exists?(user_id: user.id)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   get 'homes/about' => 'homes#about',as: 'about'
   root to: "homes#top"
-  resources :post_images, only: [:new, :create, :index, :show]
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resource :favorite, only: [:create, :destroy]
+    resources :post_comments, only: [:create, :destroy]
+  end
+  resources :users, only: [:show, :edit, :update]
   #resources :lists
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # For user on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
